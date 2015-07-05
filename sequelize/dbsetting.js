b6e8d3d510b5e65c64552649394849433090efa2
ximing <ximing@meituan.com>
2015-07-05 21:23:25 +0800

    d

--- sequelize/dbsetting.js
+++ sequelize/dbsetting.js
@@ -4,12 +4,18 @@
 
 module.exports = {
     mysql:{
-        host:"127.0.0.1",
-        user:"lyq",
-        password:'lyq001',
+        host:"210.30.100.93",
+        user:"zhouzhi",
+        password:'ximing002',
         port:"3306",
-        database:"subscribe",
+        database:"zhouzhidb",
         connectionLimit:10,
-        charset:"utf8"
+        charset:"utf8",
+        dialect: "postgres",
+      pool: {
+        "max": 100,
+        "min": 0,
+        "idle": 10000
+      }
     }
 };
